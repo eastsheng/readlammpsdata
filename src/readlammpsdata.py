4a378bc82ef6d506cc5bf43b46bbd53ead4f8734
eastsheng <eastsheng@hotmail.com>
2023-11-30 15:24:12 +0800

    fix bug

--- src/readlammpsdata.py
+++ src/readlammpsdata.py
@@ -24,10 +24,11 @@ def print_line(func):
     def wrapper(*args, **kwargs):
         print(21*"-"," Program Start ",21*"-")
         start_time = time.time()
-        func(*args, **kwargs)
+        results = func(*args, **kwargs)
         end_time = time.time()
         elapsed_time = end_time - start_time
         print(20*"-","Run time:",round(elapsed_time,2),"s ",20*"-")
+        return results
     return wrapper
 
 def extract_substring(string, char1, char2):
