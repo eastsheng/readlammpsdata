4d4fbf862fe8a637c658cf94df01adb605739103
eastsheng <eastsheng@hotmail.com>
2024-01-20 17:48:37 +0800

    add docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,15 @@ release = '1.0.9'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+	'sphinx.ext.duration',
+	'sphinx.ext.doctest',
+	'sphinx.ext.autodoc',
+	'sphinx.ext.autosummary',
+	'sphinx.ext.intersphinx',
+	'recommonmark',
+	'sphinx_markdown_tables',
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -28,3 +36,19 @@ exclude_patterns = []
 html_static_path = ['_static']
 
 html_theme = 'sphinx_rtd_theme'
+
+# -- Options for EPUB output
+epub_show_urls = 'footnote'
+
+# -- Options for PDF output
+latex_engine = 'xelatex'
+latex_use_xindy = False
+latex_elements = {
+	'preamble': '\\usepackage[UTF8]{ctex}\n',
+}
+
+source_parsers = {
+	'.md': 'recommonmark.parser.CommonMarkParser',
+}
+
+source_suffix = ['.rst', '.md']
