4cf1f0e9f68a4381f2f7e9f428141a6864bc7a3f
eastsheng <eastsheng@hotmail.com>
2024-01-20 17:14:26 +0800

    add docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,14 +25,6 @@ exclude_patterns = []
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 # html_theme = 'alabaster'
-# html_static_path = ['_static']
+html_static_path = ['_static']
 
-import sphinx_rtd_theme
-html_theme = "sphinx_rtd_theme"
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-
-from recommonmark.parser import CommonMarkParser
-source_parsers = {
-    '.md': CommonMarkParser,
-}
-source_suffix = ['.rst', '.md']
+html_theme = 'sphinx_rtd_theme'
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -1,5 +1,5 @@
 .. readlammpsdata documentation master file, created by
-   sphinx-quickstart on Sat Jan 20 14:38:13 2024.
+   sphinx-quickstart on Sat Jan 20 17:08:30 2024.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
